5c155fb2eb1a173445072b6f135d5aa6e1ee7f3b
JainiSolanki <23cs096@charusat.edu.in>
2025-08-23 18:13:39 +0530

    add post, delete post, comments

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 if(localStorage.token) {
         setAuthToken(localStorage.token);}
@@ -76,6 +77,14 @@ const App = () => {
           element={
             <PrivateRoute>
               <AddEducation />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/posts"
+          element={
+            <PrivateRoute>
+              <Posts />
             </PrivateRoute>
           }
         />
